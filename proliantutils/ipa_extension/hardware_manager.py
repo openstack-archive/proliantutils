f092ced1ac323cdd249f03dea0352a342344f988
Anusha Ramineni <anusha.iiitm@gmail.com>
2015-02-19 19:42:43 +0530

    Add Proliant Hardware Manager for IPA
    This commit adds a IPA hardware manager for
    exposing functions specific to Proliant hardware
    in Ironic Python Agent.
    
    Co-Authored-By: Ramakrishnan G <rameshg87@gmail.com>
    Implements: blueprint ipa-hardware-manager
    Change-Id: I286fcc4e88934b4a425e57f5b676d6cbd64690c4

---
+++ proliantutils/ipa_extension/hardware_manager.py
@@ -0,0 +1,50 @@
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo.concurrency import processutils
+from oslo.utils import importutils
+
+from proliantutils.hpssa import manager as hpssa_manager
+
+ironic_python_agent = importutils.try_import('ironic_python_agent')
+if ironic_python_agent:
+    from ironic_python_agent import hardware
+
+
+class ProliantHardwareManager(hardware.GenericHardwareManager):
+
+    HARDWARE_MANAGER_VERSION = "3"
+
+    def get_clean_steps(self):
+        pass
+
+    def evaluate_hardware_support(cls):
+        return hardware.HardwareSupport.SERVICE_PROVIDER
+
+    def erase_block_device(self, block_device):
+        npass = 3
+        cmd = ('shred', '--force', '--zero', '--verbose',
+               '--iterations', npass, block_device.name)
+        processutils.execute(*cmd)
+
+    def erase_devices(self):
+        block_devices = self.list_block_devices()
+        for block_device in block_devices:
+            self.erase_block_device(block_device)
+
+    def create_raid_configuration(self, raid_config):
+        return hpssa_manager.create_configuration(raid_config=raid_config)
+
+    def delete_raid_configuration(self):
+        hpssa_manager.delete_configuration()
