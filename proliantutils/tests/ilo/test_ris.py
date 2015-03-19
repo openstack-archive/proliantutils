32b0b3b493139ca1fd8efe04a8757d549155f143
Ramakrishnan G <rameshg87@gmail.com>
2015-03-19 07:45:38 +0000

    ILO: Add unit tests for ris.RISOperations._rest_op
    This commit adds the unit test cases for the main
    method ris.RISOperations._rest_op which handles the
    REST interactions with the iLO.
    
    Change-Id: I1dea76aaed8a7f92c2ccc6b91ea7cb8fc03a9969

--- proliantutils/tests/ilo/test_ris.py
+++ proliantutils/tests/ilo/test_ris.py
@@ -14,3 +14,195 @@
 #    under the License.
 
 """Test class for RIS Module."""
+
+import base64
+import httplib
+import json
+
+import mock
+import ris_sample_outputs as ris_outputs
+import testtools
+
+from proliantutils import exception
+from proliantutils.ilo import ris
+
+
+class TestRISOperationsPrivateMethods(testtools.TestCase):
+
+    def setUp(self):
+        super(TestRISOperationsPrivateMethods, self).setUp()
+        self.client = ris.RISOperations("1.2.3.4", "admin", "Admin")
+
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_okay(self, https_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        response_mock_obj = mock.MagicMock(status=200)
+        https_con_mock.return_value = connection_mock_obj
+        connection_mock_obj.getresponse.return_value = response_mock_obj
+
+        sample_response_body = ris_outputs.RESPONSE_BODY_FOR_REST_OP
+        response_mock_obj.read.return_value = sample_response_body
+
+        sample_headers = ris_outputs.HEADERS_FOR_REST_OP
+        response_mock_obj.getheaders.return_value = sample_headers
+        exp_headers = dict((x.lower(), y) for x, y in sample_headers)
+
+        status, headers, response = self.client._rest_op(
+            'GET', '/v1/foo', None, None)
+
+        self.assertEqual(200, status)
+        self.assertEqual(exp_headers, headers)
+        self.assertEqual(json.loads(sample_response_body), response)
+
+        https_con_mock.assert_called_once_with(host='1.2.3.4', strict=True)
+        connection_mock_obj.request.assert_called_once_with(
+            'GET', '/v1/foo',
+            # base64 encoded username + password for admin/Admin
+            headers={'Authorization': 'BASIC YWRtaW46QWRtaW4='},
+            body="null")
+
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_request_error(self, https_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        https_con_mock.return_value = connection_mock_obj
+        connection_mock_obj.request.side_effect = RuntimeError("boom")
+        exc = self.assertRaises(exception.IloConnectionError,
+                                self.client._rest_op,
+                                'GET', '/v1/foo', {}, None)
+        https_con_mock.assert_called_once_with(host='1.2.3.4', strict=True)
+        self.assertIn("boom", str(exc))
+
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_get_response_error(self, https_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        https_con_mock.return_value = connection_mock_obj
+        connection_mock_obj.getresponse.side_effect = RuntimeError("boom")
+        exc = self.assertRaises(exception.IloConnectionError,
+                                self.client._rest_op,
+                                'GET', '/v1/foo', None, None)
+        https_con_mock.assert_called_once_with(host='1.2.3.4', strict=True)
+        connection_mock_obj.request.assert_called_once_with(
+            'GET', '/v1/foo',
+            headers={'Authorization': 'BASIC YWRtaW46QWRtaW4='},
+            body="null")
+        self.assertIn("boom", str(exc))
+
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_response_read_error(self, https_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        response_mock_obj = mock.MagicMock(status=200)
+        https_con_mock.return_value = connection_mock_obj
+        connection_mock_obj.getresponse.return_value = response_mock_obj
+        response_mock_obj.read.side_effect = RuntimeError("boom")
+        exc = self.assertRaises(exception.IloConnectionError,
+                                self.client._rest_op,
+                                'GET', '/v1/foo', None, None)
+        self.assertIn("boom", str(exc))
+
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_continous_redirection(self, https_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        response_mock_obj = mock.MagicMock(status=301)
+        https_con_mock.side_effect = [connection_mock_obj,
+                                      connection_mock_obj,
+                                      connection_mock_obj,
+                                      connection_mock_obj,
+                                      connection_mock_obj]
+
+        connection_mock_obj.getresponse.return_value = response_mock_obj
+
+        sample_response_body = ris_outputs.RESPONSE_BODY_FOR_REST_OP
+        response_mock_obj.read.return_value = sample_response_body
+
+        sample_headers = ris_outputs.HEADERS_FOR_REST_OP
+        sample_headers.append(('location', 'https://foo'))
+        response_mock_obj.getheaders.return_value = sample_headers
+
+        exc = self.assertRaises(exception.IloConnectionError,
+                                self.client._rest_op,
+                                'GET', '/v1/foo', {}, None)
+        self.assertEqual(5, https_con_mock.call_count)
+        self.assertEqual(5, connection_mock_obj.request.call_count)
+        self.assertIn('https://1.2.3.4/v1/foo', str(exc))
+
+    @mock.patch.object(httplib, 'HTTPConnection')
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_one_redirection(self, https_con_mock,
+                                      http_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        response_mock_obj1 = mock.MagicMock(status=301)
+        response_mock_obj2 = mock.MagicMock(status=200)
+        https_con_mock.return_value = connection_mock_obj
+        http_con_mock.return_value = connection_mock_obj
+        connection_mock_obj.getresponse.side_effect = [response_mock_obj1,
+                                                       response_mock_obj2]
+
+        sample_response_body = ris_outputs.RESPONSE_BODY_FOR_REST_OP
+        response_mock_obj1.read.return_value = sample_response_body
+        response_mock_obj2.read.return_value = sample_response_body
+
+        sample_headers1 = ris_outputs.HEADERS_FOR_REST_OP
+        sample_headers2 = ris_outputs.HEADERS_FOR_REST_OP
+        sample_headers1.append(('location', 'http://5.6.7.8/v1/foo'))
+        response_mock_obj1.getheaders.return_value = sample_headers1
+        response_mock_obj2.getheaders.return_value = sample_headers2
+
+        status, headers, response = self.client._rest_op(
+            'GET', '/v1/foo', {}, None)
+
+        exp_headers = dict((x.lower(), y) for x, y in sample_headers1)
+        self.assertEqual(200, status)
+        self.assertEqual(exp_headers, headers)
+        self.assertEqual(json.loads(sample_response_body), response)
+
+        https_con_mock.assert_any_call(host='1.2.3.4', strict=True)
+        http_con_mock.assert_any_call(host='5.6.7.8', strict=True)
+        self.assertEqual(2, connection_mock_obj.request.call_count)
+        self.assertTrue(response_mock_obj1.read.called)
+        self.assertTrue(response_mock_obj2.read.called)
+
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_response_decode_error(self, https_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        response_mock_obj = mock.MagicMock(status=200)
+        https_con_mock.return_value = connection_mock_obj
+        connection_mock_obj.getresponse.return_value = response_mock_obj
+
+        sample_response_body = "{[wrong json"
+        response_mock_obj.read.return_value = sample_response_body
+
+        sample_headers = ris_outputs.HEADERS_FOR_REST_OP
+        response_mock_obj.getheaders.return_value = sample_headers
+
+        self.assertRaises(exception.IloError,
+                          self.client._rest_op,
+                          'GET', '/v1/foo', {}, None)
+        https_con_mock.assert_called_once_with(host='1.2.3.4', strict=True)
+        connection_mock_obj.request.assert_called_once_with(
+            'GET', '/v1/foo',
+            # base64 encoded username + password for admin/Admin
+            headers={'Authorization': 'BASIC YWRtaW46QWRtaW4='},
+            body="null")
+
+    @mock.patch.object(httplib, 'HTTPSConnection')
+    def test__rest_op_response_gzipped_response(self, https_con_mock):
+        connection_mock_obj = mock.MagicMock()
+        response_mock_obj = mock.MagicMock(status=200)
+        https_con_mock.return_value = connection_mock_obj
+        connection_mock_obj.getresponse.return_value = response_mock_obj
+
+        sample_response_body = ris_outputs.RESPONSE_BODY_FOR_REST_OP
+        gzipped_response_body = base64.b64decode(
+            ris_outputs.BASE64_GZIPPED_RESPONSE)
+        response_mock_obj.read.return_value = gzipped_response_body
+
+        sample_headers = ris_outputs.HEADERS_FOR_REST_OP
+        response_mock_obj.getheaders.return_value = sample_headers
+        exp_headers = dict((x.lower(), y) for x, y in sample_headers)
+
+        status, headers, response = self.client._rest_op(
+            'GET', '/v1/foo', {}, None)
+
+        self.assertEqual(200, status)
+        self.assertEqual(exp_headers, headers)
+        self.assertEqual(json.loads(sample_response_body), response)
