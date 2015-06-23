0bc40bb1feaa982bff3c69ff23e4cd62ff39e6aa
Anusha Ramineni <anusha.iiitm@gmail.com>
2015-06-23 21:32:52 +0530

    Common Changes required for firmware update
    This commit includes the common changes for firmware update used
    by RIS and RIBCL.
    
    Change-Id: I3fc0b541222e55e737336c5163626dffa8a56a94

--- proliantutils/ilo/common.py
+++ proliantutils/ilo/common.py
@@ -15,12 +15,19 @@
 """Common functionalities used by both RIBCL and RIS."""
 
 import time
+import glob
+import os.path
+import re
+import stat
 
+from oslo.concurrency import processutils as utils
 from proliantutils import exception
 
 # Max number of times an operation to be retried
 RETRY_COUNT = 10
-
+TEMP_DIR = '/tmp'
+CHUNK_SIZE = 1024*1024
+SUPPORTED_FW_EXTNS = ['hex', 'bin', 'vme']
 
 def wait_for_ilo_after_reset(ilo_object):
     """Checks if iLO is up after reset."""
@@ -39,3 +46,71 @@ def wait_for_ilo_after_reset(ilo_object):
     else:
         msg = ('iLO is not up after reset.')
         raise exception.IloConnectionError(msg)
+
+def _parse_firmware_url(url):
+    """Checks if its a valid url and extracts it."""
+    filename = os.path.basename(url)
+    name, ext = os.path.splitext(filename)
+    if not any(e in ext for e in ('scexe','rpm')):
+        raise exception.InvalidInputError("Not a valid file")
+    #TODO:Need to also categorize .bin, .hex or .Bxx as valid files
+    return name, ext
+
+def extract(location):
+    """Extract the file based on extension.
+
+    :param location: path of the file to be extracted
+    :returns: extracted location of the file
+    """
+    # Add executable permissions to the file
+    mode = os.stat(location).st_mode
+    os.chmod(location, mode| stat.S_IXUSR)
+
+    name, ext = _parse_firmware_url(location)
+    path_extract = os.path.join(TEMP_DIR, name)
+
+    if 'scexe' in ext:
+        return _extract_scexe(location, path_extract)
+    elif 'rpm' in ext:
+        # Create a temp dir to extract rpm
+        if not os.path.exists(path_extract):
+            os.makedirs(path_extract)
+        return _extract_rpm(location, path_extract)
+    else:
+        # It should be a direct .bin or .hex or .Bxx file
+        return location
+
+def _extract_scexe(target_file, extract_path):
+    """ Extract the scexe file """
+    # Command to extract the smart component file.
+    unpack_cmd = '--unpack='+ extract_path
+    args = [target_file, unpack_cmd]
+    try:
+        out, err = utils.execute(*args)
+    except Exception as err_msg:
+        raise exception.IloError(err_msg)
+
+    filename = _validate_and_get_firmware_file(extract_path)
+    return filename
+
+def _validate_and_get_firmware_file(path):
+    """Parses the extract directory and get the firmware file."""
+    for root, dirnames, filenames in os.walk(path):
+        for name in filenames:
+            ext = os.path.splitext(os.path.basename(name))[1][1::]
+            if ext:
+                for e in SUPPORTED_FW_EXTNS:
+                    if re.match(e, ext):
+                        return name
+
+def _extract_rpm(target_file, extract_path=None):
+    """Using rpm2cpio command to extract rpm file"""
+    os.chdir(extract_path)
+    cmd = ['rpm2cpio', target_file, '|', 'cpio', '-idm']
+    try:
+        out, err = utils.execute(*cmd)
+    except Exception as err_msg:
+        raise exception.IloError(err_msg)
+
+    filename = _validate_and_get_firmware_file(extract_path)
+    return filename
