3993c7d5cf62b664550a170a52b804b27fdad3cd
kesper <paresh.h.sao@gmail.com>
2016-06-13 11:24:05 +0530

    Adds constructor for InvalidInputError
    This change allows keyword arguments to constructor of
    InvalidInputError exception. And supports the use of ``reason``
    as well as user-defined arguments as part of the error message.
    
    Closes-bug: #1546878
    Co-Authored-By: Debayan Ray <debayan.ray@gmail.com>
    Change-Id: I99d901b58e45a90be128f1006a447ce69ebe91a1

---
+++ proliantutils/tests/test_exception.py
@@ -0,0 +1,60 @@
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""Test Class for Log."""
+
+import unittest
+
+from proliantutils import exception
+
+
+class ExceptionTestCase(unittest.TestCase):
+
+    def setUp(self):
+        super(ExceptionTestCase, self).setUp()
+
+    def test_InvalidInputError(self):
+        # | GIVEN |
+        def raising_invalid_input_error_exc_with(msg, **kwargs):
+            raise exception.InvalidInputError(msg, **kwargs)
+
+        # | WHEN | & | THEN |
+        self.assertRaisesRegexp(exception.InvalidInputError,
+                                'check your input',
+                                raising_invalid_input_error_exc_with,
+                                'check your input')
+        self.assertRaisesRegexp(exception.InvalidInputError,
+                                'check your input. invalid input provided',
+                                raising_invalid_input_error_exc_with,
+                                'check your input. %(reason)s',
+                                reason='invalid input provided')
+        self.assertRaisesRegexp(exception.InvalidInputError,
+                                "just check your input.. that's all",
+                                raising_invalid_input_error_exc_with,
+                                "just check your input.. that's all",
+                                reason='invalid input provided')
+        self.assertRaisesRegexp(exception.InvalidInputError,
+                                'Invalid Input: Input not supported',
+                                raising_invalid_input_error_exc_with,
+                                None,
+                                reason='Input not supported')
+        self.assertRaisesRegexp(exception.InvalidInputError,
+                                'check your input. invalid input provided',
+                                raising_invalid_input_error_exc_with,
+                                'check your input. %(why)s',
+                                why='invalid input provided')
+        self.assertRaisesRegexp(exception.InvalidInputError,
+                                'Invalid Input: Unknown',
+                                raising_invalid_input_error_exc_with,
+                                None)
