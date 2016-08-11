d7e6cd0447ce28d4470d344c17b43f338b020b02
ankit <ankit.dhn31@gmail.com>
2016-08-11 07:00:20 +0000

    Fix log message for exception in Gen7
    Previously the exception raised for Gen7 servers were
    not handled properly.
    
    This commit resolves the issue by handling the exception
    in a appropriate manner.
    
    Change-Id: I7371fb26e78a558ee382bbe9785e6401c51437bd
    Closes-Bug: #1613831

--- proliantutils/ilo/ris.py
+++ proliantutils/ilo/ris.py
@@ -120,8 +120,7 @@ class RISOperations(operations.IloOperations):
             try:
                 response = request_method(url.geturl(), **kwargs)
             except Exception as e:
-                LOG.exception(self._("An error occurred while "
-                                     "contacting iLO. Error: %s"), e)
+                LOG.debug(self._("Unable to connect to iLO"), e)
                 raise exception.IloConnectionError(e)
 
             # NOTE:Do not assume every HTTP operation will return a JSON body.
@@ -142,9 +141,9 @@ class RISOperations(operations.IloOperations):
         else:
             # Redirected for 5th time. Throw error
             msg = (self._("URL Redirected 5 times continuously. "
-                          "URL incorrect: %(start_url)s") %
+                          "URL used: %(start_url)s") %
                    {'start_url': start_url})
-            LOG.error(msg)
+            LOG.debug(msg)
             raise exception.IloConnectionError(msg)
 
         response_body = {}
@@ -167,7 +166,7 @@ class RISOperations(operations.IloOperations):
                     uncompressed_string = gzipper.read().decode('UTF-8')
                     response_body = json.loads(uncompressed_string)
                 except Exception as e:
-                    LOG.error(self._("Got invalid response "
+                    LOG.debug(self._("Got invalid response "
                                      "'%(response)s' for url %(url)s."),
                               {'url': url.geturl(),
                                'response': response.text})
@@ -1562,8 +1561,8 @@ class RISOperations(operations.IloOperations):
             return
 
         if state == "ERROR":
-            msg = 'Error in firmware update'
-            LOG.error(self._(msg))  # noqa
+            msg = 'Unable to update firmware'
+            LOG.debug(self._(msg))  # noqa
             raise exception.IloError(msg)
         elif state == "UNKNOWN":
             msg = 'Status of firmware update not known'
