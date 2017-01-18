8ff3b4f3bc06fef6ad0da509256cf95c9dc60a21
Aparna <aparnavtce@gmail.com>
2017-01-18 12:46:44 +0000

    Add HPSUM firmware update support
    This commit adds the support to perform hpsum
    firmware update on the node using SPP (Service
    Pack for Proliant) iso.
    
    Closes-Bug: #1657492
    Change-Id: If76ae30f4007cb468fabf95d110cd16f272f8bcd

--- proliantutils/exception.py
+++ proliantutils/exception.py
@@ -172,3 +172,32 @@ class IloSNMPExceptionFailure(IloError):
             message = self.message % kwargs
 
         super(IloSNMPExceptionFailure, self).__init__(message)
+
+
+class ImageRefValidationFailed(ProliantUtilsException):
+    message = ("Validation of image href %(image_href)s failed, "
+               "reason: %(reason)s")
+
+    def __init__(self, message=None, **kwargs):
+        if not message:
+            message = self.message % kwargs
+
+        super(ImageRefValidationFailed, self).__init__(message)
+
+
+class HpsumOperationError(ProliantUtilsException):
+    """Hpsum based firmware update operation error.
+
+    This exception is used when a problem is encountered in
+    executing a hpsum operation.
+    """
+
+    message = ("An error occurred while performing hpsum based firmware "
+               "update")
+
+    def __init__(self, message=None, **kwargs):
+        if not message:
+            message = self.message
+
+        message = message % kwargs
+        super(HpsumOperationError, self).__init__(message)
