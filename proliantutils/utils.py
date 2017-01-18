e214ef89f47abd43b3535e41341db1a1850541f3
Aparna <aparnavtce@gmail.com>
2017-01-18 12:46:44 +0000

    Add HPSUM firmware update support
    This commit adds the support to perform hpsum
    firmware update on the node using SPP (Service
    Pack for Proliant) iso.
    
    Closes-Bug: #1657492
    Change-Id: If76ae30f4007cb468fabf95d110cd16f272f8bcd

--- proliantutils/utils.py
+++ proliantutils/utils.py
@@ -15,7 +15,13 @@
 """
 Non-iLO related utilities and helper functions.
 """
+import hashlib
 
+import requests
+import six
+from six.moves import http_client
+
+from proliantutils import exception
 from proliantutils.ilo import firmware_controller
 from proliantutils import log
 
@@ -63,3 +69,86 @@ def process_firmware_image(compact_firmware_file, ilo_object):
             'yes_or_no': 'Yes' if to_upload else 'No'})
     LOG.info(msg)
     return raw_fw_file_path, to_upload, is_extracted
+
+
+def _get_hash_object(hash_algo_name):
+    """Create a hash object based on given algorithm.
+
+    :param hash_algo_name: name of the hashing algorithm.
+    :raises: InvalidInputError, on unsupported or invalid input.
+    :returns: a hash object based on the given named algorithm.
+    """
+    algorithms = (hashlib.algorithms_guaranteed if six.PY3
+                  else hashlib.algorithms)
+    if hash_algo_name not in algorithms:
+        msg = ("Unsupported/Invalid hash name '%s' provided."
+               % hash_algo_name)
+        raise exception.InvalidInputError(msg)
+
+    return getattr(hashlib, hash_algo_name)()
+
+
+def hash_file(file_like_object, hash_algo='md5'):
+    """Generate a hash for the contents of a file.
+
+    It returns a hash of the file object as a string of double length,
+    containing only hexadecimal digits. It supports all the algorithms
+    hashlib does.
+    :param file_like_object: file like object whose hash to be calculated.
+    :param hash_algo: name of the hashing strategy, default being 'md5'.
+    :raises: InvalidInputError, on unsupported or invalid input.
+    :returns: a condensed digest of the bytes of contents.
+    """
+    checksum = _get_hash_object(hash_algo)
+    for chunk in iter(lambda: file_like_object.read(32768), b''):
+        checksum.update(chunk)
+    return checksum.hexdigest()
+
+
+def verify_image_checksum(image_location, expected_checksum):
+    """Verifies checksum (md5) of image file against the expected one.
+
+    This method generates the checksum of the image file on the fly and
+    verifies it against the expected checksum provided as argument.
+
+    :param image_location: location of image file whose checksum is verified.
+    :param expected_checksum: checksum to be checked against
+    :raises: ImageRefValidationFailed, if invalid file path or
+             verification fails.
+    """
+    try:
+        with open(image_location, 'rb') as fd:
+            actual_checksum = hash_file(fd)
+    except IOError as e:
+        raise exception.ImageRefValidationFailed(image_href=image_location,
+                                                 reason=e)
+
+    if actual_checksum != expected_checksum:
+        msg = ('Error verifying image checksum. Image %(image)s failed to '
+               'verify against checksum %(checksum)s. Actual checksum is: '
+               '%(actual_checksum)s' %
+               {'image': image_location, 'checksum': expected_checksum,
+                'actual_checksum': actual_checksum})
+        raise exception.ImageRefValidationFailed(image_href=image_location,
+                                                 reason=msg)
+
+
+def validate_href(image_href):
+        """Validate HTTP image reference.
+
+        :param image_href: Image reference.
+        :raises: exception.ImageRefValidationFailed if HEAD request failed or
+            returned response code not equal to 200.
+        :returns: Response to HEAD request.
+        """
+        try:
+            response = requests.head(image_href)
+            if response.status_code != http_client.OK:
+                raise exception.ImageRefValidationFailed(
+                    image_href=image_href,
+                    reason=("Got HTTP code %s instead of 200 in response to "
+                            "HEAD request." % response.status_code))
+        except requests.RequestException as e:
+            raise exception.ImageRefValidationFailed(image_href=image_href,
+                                                     reason=e)
+        return response
