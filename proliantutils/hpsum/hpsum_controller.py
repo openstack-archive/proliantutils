ab474cbd61f3f02c2a98c9b11158d94632faa2dd
Aparna <aparnavtce@gmail.com>
2017-01-18 12:46:44 +0000

    Add HPSUM firmware update support
    This commit adds the support to perform hpsum
    firmware update on the node using SPP (Service
    Pack for Proliant) iso.
    
    Closes-Bug: #1657492
    Change-Id: If76ae30f4007cb468fabf95d110cd16f272f8bcd

---
+++ proliantutils/hpsum/hpsum_controller.py
@@ -0,0 +1,196 @@
+# Copyright 2017 Hewlett Packard Enterprise Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import fnmatch
+import os
+import re
+import shutil
+import tempfile
+import time
+
+from oslo_concurrency import processutils
+
+from proliantutils import exception
+from proliantutils.ilo import client
+from proliantutils import utils
+
+
+OUTPUT_FILE = '/var/hp/log/localhost/hpsum_log.txt'
+
+HPSUM_LOCATION = 'hp/swpackages/hpsum'
+
+EXIT_CODE_TO_STRING = {
+    0: "The smart component was installed successfully.",
+    1: ("The smart component was installed successfully, but the system "
+        "must be restarted."),
+    3: ("The smart component was not installed. Node is already "
+        "up-to-date."),
+    253: "The installation of the component failed."
+    }
+
+
+def _execute_hpsum(hpsum_file_path, components):
+    """Executes the hpsum firmware update command.
+
+    This method executes the hpsum firmware update command to update the
+    components specified, if not, it performs update on all the firmware
+    components on th server.
+
+    :param hpsum_file_path: A string with the path to the hpsum binary to be
+        executed
+    :param components: A list of components to be updated. If it is None, all
+        the firmware components are updated.
+    :raises: HpsumOperationError, when the hpsum firmware update operation on
+        the node fails.
+    """
+    cmd = ' --c ' + ' --c '.join(components) if components else ''
+
+    try:
+        processutils.execute(hpsum_file_path, "--s", "--romonly", cmd)
+    except processutils.ProcessExecutionError as e:
+        result = _parse_hpsum_ouput(e.exit_code)
+        if result:
+            return result
+        else:
+            msg = ("Unable to perform hpsum firmware update on the node. "
+                   "Error: " + str(e))
+            raise exception.HpsumOperationError(msg)
+
+
+def _parse_hpsum_ouput(exit_code):
+    """Parse the hpsum output log file.
+
+    This method parses through the hpsum log file in the
+    default location to return the hpsum update status.
+
+    :param exit_code: A integer returned by the hpsum after command
+         execution.
+    :returns: A string with the statistics of the updated/failed
+        components.
+    :raises: HpsumOperationError, when the hpsum log file does not
+        exists.
+    """
+    if exit_code == 3:
+        return "SUMMARY: %s" % EXIT_CODE_TO_STRING.get(exit_code)
+
+    if exit_code in (0, 1, 253):
+        if os.path.exists(OUTPUT_FILE):
+            with open(OUTPUT_FILE, 'r') as f:
+                output_data = f.read()
+
+            ret_data = output_data[(output_data.find('Deployed Components:') +
+                                    len('Deployed Components:')):
+                                   output_data.find('Exit status:')]
+
+            failed = 0
+            success = 0
+            for line in re.split('\n\n', ret_data):
+                if line:
+                    if 'Success' not in line:
+                        failed += 1
+                    else:
+                        success += 1
+
+            result = ("SUMMARY: %(return_code)s STATUS of updated components:"
+                      " TOTAL: %(total)s SUCCESS: %(success)s" %
+                      {'return_code': EXIT_CODE_TO_STRING.get(exit_code),
+                       'total': (success + failed), 'success': success})
+            if failed != 0:
+                result = result + " FAILED: %s" % failed
+            return result
+
+        return "UPDATE STATUS: UNKNOWN"
+
+
+def update_firmware(node):
+    """Performs hpsum firmware update on the node.
+
+    This method performs hpsum firmware update by mounting the
+    SPP ISO on the node. It performs firmware update on all or
+    some of the firmware components.
+
+    :param node: A node object of type dict.
+    :returns: Operation Status string.
+    :raises: HpsumOperationError, when the vmedia device is not found or
+        when the mount operation fails or when the image validation fails.
+    :raises: IloConnectionError, when the iLO connection fails.
+    :raises: IloError, when vmedia eject or insert operation fails.
+    """
+    hpsum_update_iso = node['clean_step']['args']['firmware_images'][0].get(
+        'url')
+
+    # Validates the http image reference for hpsum update ISO.
+    try:
+        utils.validate_href(hpsum_update_iso)
+    except exception.ImageRefValidationFailed as e:
+        raise exception.HpsumOperationError(e)
+
+    # Ejects the CDROM device in the iLO and inserts the hpsum update ISO
+    # to the CDROM device.
+    info = node.get('driver_info')
+    ilo_object = client.IloClient(info.get('ilo_address'),
+                                  info.get('ilo_username'),
+                                  info.get('ilo_password'))
+
+    ilo_object.eject_virtual_media('CDROM')
+    ilo_object.insert_virtual_media(hpsum_update_iso, 'CDROM')
+
+    # Waits for the OS to detect the disk and update the label file. SPP ISO
+    # is identified by matching its label.
+    time.sleep(5)
+    vmedia_device_dir = "/dev/disk/by-label/"
+    for file in os.listdir(vmedia_device_dir):
+        if fnmatch.fnmatch(file, 'SPP*'):
+            vmedia_device_file = os.path.join(vmedia_device_dir, file)
+
+    if not os.path.exists(vmedia_device_file):
+        msg = "Unable to find the virtual media device for HPSUM"
+        raise exception.HpsumOperationError(msg)
+
+    # Validates the SPP ISO image for any file corruption using the checksum
+    # of the ISO file.
+    expected_checksum = node['clean_step']['args']['firmware_images'][0].get(
+        'checksum')
+    try:
+        utils.verify_image_checksum(vmedia_device_file, expected_checksum)
+    except exception.ImageRefValidationFailed as e:
+        raise exception.HpsumOperationError(e)
+
+    # Mounts SPP ISO on a temporary directory.
+    vmedia_mount_point = tempfile.mkdtemp()
+    try:
+        try:
+            processutils.execute("mount", vmedia_device_file,
+                                 vmedia_mount_point)
+        except processutils.ProcessExecutionError as e:
+            msg = ("Unable to mount virtual media device %(device)s: "
+                   "%(error)s" % {'device': vmedia_device_file, 'error': e})
+            raise exception.HpsumOperationError(msg)
+
+        # Executes the hpsum based firmware update by passing the default hpsum
+        # executable path and the components specified, if any.
+        hpsum_file_path = os.path.join(vmedia_mount_point, HPSUM_LOCATION)
+        components = node['clean_step']['args']['firmware_images'][0].get(
+            'component')
+        if components:
+            components = components.strip().split(',')
+
+        result = _execute_hpsum(hpsum_file_path, components=components)
+
+        processutils.trycmd("umount", vmedia_mount_point)
+    finally:
+        shutil.rmtree(vmedia_mount_point, ignore_errors=True)
+
+    return result
---
+++ proliantutils/tests/hpsum/test_hpsum_controller.py
@@ -0,0 +1,255 @@
+# Copyright 2017 Hewlett Packard Enterprise Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import shutil
+import tempfile
+
+import mock
+from oslo_concurrency import processutils
+import testtools
+
+from proliantutils import exception
+from proliantutils.hpsum import hpsum_controller
+from proliantutils.ilo import client as ilo_client
+from proliantutils.tests.hpsum import hpsum_sample_output as constants
+from proliantutils import utils
+
+
+class HpsumFirmwareUpdateTest(testtools.TestCase):
+
+    def setUp(self):
+        super(HpsumFirmwareUpdateTest, self).setUp()
+        self.info = {'ilo_address': '1.2.3.4',
+                     'ilo_password': '12345678',
+                     'ilo_username': 'admin'}
+        clean_step = {
+            'interface': 'management',
+            'step': 'update_firmware',
+            'args': {'firmware_update_mode': u'hpsum',
+                     'firmware_images': [{'url': 'http://1.2.3.4/SPP.iso',
+                                          'checksum': '1234567890'}]}}
+        self.node = {'driver_info': self.info,
+                     'clean_step': clean_step}
+
+    @mock.patch.object(hpsum_controller, 'open',
+                       mock.mock_open(read_data=constants.HPSUM_OUTPUT_DATA))
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch.object(processutils, 'execute')
+    def test_execute_hpsum(self, execute_mock, exists_mock):
+        exists_mock.return_value = True
+        value = ("hpsum_service_x64 started successfully. Sending Shutdown "
+                 "request to engine. Successfully shutdown the service.")
+        execute_mock.side_effect = processutils.ProcessExecutionError(
+            stdout=value, stderr=None, exit_code=0)
+        ret_value = ("SUMMARY: The smart component was installed successfully."
+                     " STATUS of updated components: TOTAL: 2 SUCCESS: 2")
+
+        stdout = hpsum_controller._execute_hpsum("hpsum", components=None)
+
+        self.assertEqual(ret_value, stdout)
+        execute_mock.assert_called_once_with("hpsum", "--s", "--romonly", "")
+
+    @mock.patch.object(processutils, 'execute')
+    def test_execute_hpsum_with_args(self, execute_mock):
+        value = ("hpsum_service_x64 started successfully. Sending Shutdown "
+                 "request to engine. Successfully shutdown the service.")
+        execute_mock.side_effect = processutils.ProcessExecutionError(
+            stdout=value, stderr=None, exit_code=3)
+        ret_value = ("SUMMARY: The smart component was not installed. Node is "
+                     "already up-to-date.")
+
+        stdout = hpsum_controller._execute_hpsum("hpsum",
+                                                 components=["foo", "bar"])
+
+        execute_mock.assert_called_once_with(
+            "hpsum", "--s", "--romonly", " --c foo --c bar")
+        self.assertEqual(ret_value, stdout)
+
+    @mock.patch.object(
+        hpsum_controller, 'open',
+        mock.mock_open(read_data=constants.HPSUM_OUTPUT_DATA_FAILURE))
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch.object(processutils, 'execute')
+    def test_execute_hpsum_update_fails(self, execute_mock, exists_mock):
+        exists_mock.return_value = True
+        ret = ("SUMMARY: The installation of the component failed. STATUS "
+               "of updated components: TOTAL: 2 SUCCESS: 1 FAILED: 1")
+        value = ("hpsum_service_x64 started successfully. Sending Shutdown "
+                 "request to engine. Successfully shutdown the service.")
+        execute_mock.side_effect = processutils.ProcessExecutionError(
+            stdout=value, stderr=None, exit_code=253)
+
+        stdout = hpsum_controller._execute_hpsum("hpsum", components=None)
+
+        self.assertEqual(ret, stdout)
+        execute_mock.assert_called_once_with("hpsum", "--s", "--romonly", "")
+
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch.object(processutils, 'execute')
+    def test_execute_hpsum_fails(self, execute_mock, exists_mock):
+        exists_mock.return_value = False
+        value = ("Error: Cannot launch hpsum_service_x64 locally. Reason: "
+                 "General failure.")
+        execute_mock.side_effect = processutils.ProcessExecutionError(
+            stdout=value, stderr=None, exit_code=255)
+
+        ex = self.assertRaises(exception.HpsumOperationError,
+                               hpsum_controller._execute_hpsum, "hpsum",
+                               None)
+        self.assertIn(value, str(ex))
+
+    @mock.patch.object(utils, 'validate_href')
+    @mock.patch.object(utils, 'verify_image_checksum')
+    @mock.patch.object(hpsum_controller, '_execute_hpsum')
+    @mock.patch.object(os, 'listdir')
+    @mock.patch.object(shutil, 'rmtree', autospec=True)
+    @mock.patch.object(tempfile, 'mkdtemp', autospec=True)
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch.object(os, 'mkdir')
+    @mock.patch.object(processutils, 'execute')
+    @mock.patch.object(ilo_client, 'IloClient', spec_set=True, autospec=True)
+    def test_update_firmware(self, client_mock, execute_mock, mkdir_mock,
+                             exists_mock, mkdtemp_mock, rmtree_mock,
+                             listdir_mock, execute_hpsum_mock,
+                             verify_image_mock, validate_mock):
+        ilo_mock_object = client_mock.return_value
+        eject_media_mock = ilo_mock_object.eject_virtual_media
+        insert_media_mock = ilo_mock_object.insert_virtual_media
+        execute_hpsum_mock.return_value = 'SUCCESS'
+        listdir_mock.return_value = ['SPP_LABEL']
+        mkdtemp_mock.return_value = "/tempdir"
+        null_output = ["", ""]
+        exists_mock.side_effect = [True, False]
+        execute_mock.side_effect = [null_output, null_output]
+
+        ret_val = hpsum_controller.update_firmware(self.node)
+
+        eject_media_mock.assert_called_once_with('CDROM')
+        insert_media_mock.assert_called_once_with('http://1.2.3.4/SPP.iso',
+                                                  'CDROM')
+        execute_mock.assert_any_call('mount', "/dev/disk/by-label/SPP_LABEL",
+                                     "/tempdir")
+        execute_hpsum_mock.assert_any_call('/tempdir/hp/swpackages/hpsum',
+                                           components=None)
+        exists_mock.assert_called_once_with("/dev/disk/by-label/SPP_LABEL")
+        execute_mock.assert_any_call('umount', "/tempdir")
+        mkdtemp_mock.assert_called_once_with()
+        rmtree_mock.assert_called_once_with("/tempdir", ignore_errors=True)
+        self.assertEqual('SUCCESS', ret_val)
+
+    @mock.patch.object(utils, 'validate_href')
+    @mock.patch.object(ilo_client, 'IloClient', spec_set=True, autospec=True)
+    def test_update_firmware_vmedia_attach_fails(self, client_mock,
+                                                 validate_mock):
+        ilo_mock_object = client_mock.return_value
+        eject_media_mock = ilo_mock_object.eject_virtual_media
+        value = ("Unable to attach hpsum SPP iso http://1.2.3.4/SPP.iso "
+                 "to the iLO")
+        eject_media_mock.side_effect = exception.IloError(value)
+
+        exc = self.assertRaises(exception.IloError,
+                                hpsum_controller.update_firmware, self.node)
+        self.assertEqual(value, str(exc))
+
+    @mock.patch.object(utils, 'validate_href')
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch.object(os, 'listdir')
+    @mock.patch.object(ilo_client, 'IloClient', spec_set=True, autospec=True)
+    def test_update_firmware_device_file_not_found(self, client_mock,
+                                                   listdir_mock, exists_mock,
+                                                   validate_mock):
+        ilo_mock_object = client_mock.return_value
+        eject_media_mock = ilo_mock_object.eject_virtual_media
+        insert_media_mock = ilo_mock_object.insert_virtual_media
+
+        listdir_mock.return_value = ['SPP_LABEL']
+        exists_mock.return_value = False
+
+        msg = "Unable to find the virtual media device for HPSUM"
+        exc = self.assertRaises(exception.HpsumOperationError,
+                                hpsum_controller.update_firmware, self.node)
+        self.assertEqual(msg, str(exc))
+        eject_media_mock.assert_called_once_with('CDROM')
+        insert_media_mock.assert_called_once_with('http://1.2.3.4/SPP.iso',
+                                                  'CDROM')
+        exists_mock.assert_called_once_with("/dev/disk/by-label/SPP_LABEL")
+
+    @mock.patch.object(utils, 'validate_href')
+    @mock.patch.object(utils, 'verify_image_checksum')
+    @mock.patch.object(processutils, 'execute')
+    @mock.patch.object(tempfile, 'mkdtemp', autospec=True)
+    @mock.patch.object(os, 'mkdir')
+    @mock.patch.object(os.path, 'exists')
+    @mock.patch.object(os, 'listdir')
+    @mock.patch.object(ilo_client, 'IloClient', spec_set=True, autospec=True)
+    def test_update_firmware_mount_fails(self, client_mock, listdir_mock,
+                                         exists_mock, mkdir_mock,
+                                         mkdtemp_mock, execute_mock,
+                                         verify_image_mock, validate_mock):
+        ilo_mock_object = client_mock.return_value
+        eject_media_mock = ilo_mock_object.eject_virtual_media
+        insert_media_mock = ilo_mock_object.insert_virtual_media
+        listdir_mock.return_value = ['SPP_LABEL']
+        exists_mock.return_value = True
+        mkdtemp_mock.return_value = "/tempdir"
+        execute_mock.side_effect = processutils.ProcessExecutionError
+
+        msg = ("Unable to mount virtual media device "
+               "/dev/disk/by-label/SPP_LABEL")
+        exc = self.assertRaises(exception.HpsumOperationError,
+                                hpsum_controller.update_firmware, self.node)
+        self.assertIn(msg, str(exc))
+        eject_media_mock.assert_called_once_with('CDROM')
+        insert_media_mock.assert_called_once_with('http://1.2.3.4/SPP.iso',
+                                                  'CDROM')
+        exists_mock.assert_called_once_with("/dev/disk/by-label/SPP_LABEL")
+
+    @mock.patch.object(hpsum_controller, 'open',
+                       mock.mock_open(read_data=constants.HPSUM_OUTPUT_DATA))
+    @mock.patch.object(os.path, 'exists')
+    def test__parse_hpsum_ouput(self, exists_mock):
+        exists_mock.return_value = True
+        expt_ret = ("SUMMARY: The smart component was installed successfully. "
+                    "STATUS of updated components: TOTAL: 2 SUCCESS: 2")
+
+        ret = hpsum_controller._parse_hpsum_ouput(0)
+
+        exists_mock.assert_called_once_with(hpsum_controller.OUTPUT_FILE)
+        self.assertEqual(expt_ret, ret)
+
+    @mock.patch.object(
+        hpsum_controller, 'open',
+        mock.mock_open(read_data=constants.HPSUM_OUTPUT_DATA_FAILURE))
+    @mock.patch.object(os.path, 'exists')
+    def test__parse_hpsum_ouput_some_failed(self, exists_mock):
+        exists_mock.return_value = True
+        expt_ret = ("SUMMARY: The installation of the component failed. "
+                    "STATUS of updated components: TOTAL: 2 SUCCESS: 1 "
+                    "FAILED: 1")
+
+        ret = hpsum_controller._parse_hpsum_ouput(253)
+
+        exists_mock.assert_called_once_with(hpsum_controller.OUTPUT_FILE)
+        self.assertEqual(expt_ret, ret)
+
+    @mock.patch.object(os.path, 'exists')
+    def test__parse_hpsum_ouput_fails(self, exists_mock):
+        exists_mock.return_value = False
+        expt_ret = ("UPDATE STATUS: UNKNOWN")
+
+        ret = hpsum_controller._parse_hpsum_ouput(1)
+
+        exists_mock.assert_called_once_with(hpsum_controller.OUTPUT_FILE)
+        self.assertEqual(expt_ret, ret)
