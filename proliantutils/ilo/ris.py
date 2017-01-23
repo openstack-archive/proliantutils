c1eb48d82ee7fbae3a46d6855345396266d50177
Nisha Agarwal <agarwalnisha1980@gmail.com>
2017-01-23 10:14:29 +0000

    Vendor gpu count
    This patch gets the total gpu devices per vendor
    and add that to the capabilities.
    
    Change-Id: I75ce64280cd0f02294630c784d26594ab73e6fe6

--- proliantutils/ilo/ris.py
+++ proliantutils/ilo/ris.py
@@ -48,6 +48,11 @@ POWER_STATE = {
     'OFF': 'ForceOff',
 }
 
+MAP_VENDORID_TO_VENDOR = {
+    '0x10DE': 'NVIDIA',
+    '0x1002': 'AMD',
+    '0x8086': 'Intel'
+}
 # The PCI standards mention following categories of PCI devices as
 # GPU devices.
 # Base Class Code 03 indicate VGA devices
@@ -1131,8 +1136,11 @@ class RISOperations(operations.IloOperations):
             system['Oem']['Hp']['Bios']['Current']['VersionString'])
         capabilities['rom_firmware_version'] = rom_firmware_version
         capabilities.update(self._get_ilo_firmware_version())
-        capabilities.update(self._get_number_of_gpu_devices_connected())
         capabilities.update(self._get_tpm_capability())
+        gpu_count_dict = self._get_vendor_gpu_count()
+        capabilities.update(
+            {'pci_gpu_devices': gpu_count_dict['pci_gpu_devices']})
+        capabilities.update(gpu_count_dict['count'])
         try:
             self.get_secure_boot_mode()
             capabilities['secure_boot'] = 'true'
@@ -1648,12 +1656,6 @@ class RISOperations(operations.IloOperations):
                   fw_update_progress_percent)
         return fw_update_state, fw_update_progress_percent
 
-    def _get_number_of_gpu_devices_connected(self):
-        """get the number of GPU devices connected."""
-        gpu_devices = self._get_gpu_pci_devices()
-        gpu_devices_count = len(gpu_devices)
-        return {'pci_gpu_devices': gpu_devices_count}
-
     def _get_tpm_capability(self):
         """Retrieves if server is TPM capable or not.
 
@@ -1669,3 +1671,20 @@ class RISOperations(operations.IloOperations):
         tpm_result = tpm_values[tpm_state]
 
         return {'trusted_boot': tpm_result}
+
+    def _get_vendor_gpu_count(self):
+        """Get the total gpu count per vendor."""
+        gpu_devices = self._get_gpu_pci_devices()
+        count = {}
+        for item in gpu_devices:
+            vendor_id = hex(item['VendorID'])
+            try:
+                vendor = MAP_VENDORID_TO_VENDOR[vendor_id]
+            except KeyError:
+                vendor = vendor_id
+            vendor_var_name = vendor + "_gpu_count"
+            if not count.get(vendor_var_name):
+                count[vendor_var_name] = 0
+            count[vendor_var_name] = count.get(vendor_var_name) + 1
+        gpu_devices_count = len(gpu_devices)
+        return {'pci_gpu_devices': gpu_devices_count, 'count': count}
