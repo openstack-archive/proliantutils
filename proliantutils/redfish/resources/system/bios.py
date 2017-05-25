8ad56ead0de9fcde8ffd380d05aa38d493dae55c
kesper <paresh.h.sao@gmail.com>
2017-05-25 10:16:25 +0000

    Adds 'get_pending_boot_mode' in ILO redfish
    This commits adds changes to get pending boot mode
    in ILO through redfish.
    
    Change-Id: If4f2b5ef9fff1c25b233621004c1a4efa2dd0f64

---
+++ proliantutils/redfish/resources/system/bios.py
@@ -0,0 +1,48 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from sushy.resources import base
+
+
+class BiosResource(base.ResourceBase):
+
+    BootMode = base.Field(["Attributes", "BootMode"])
+    bios_settings_odataid = base.Field(["@Redfish.Settings", "SettingsObject",
+                                 "@odata.id"])
+
+    def __init__(self, connector, identity, redfish_version=None):
+        """A class representing a BootResource
+
+        :param connector: A Connector instance
+        :param identity: The identity of the BootResource
+        :param redfish_version: The version of RedFish. Used to construct
+            the object according to schema of the given version.
+        """
+        super(BiosResource, self).__init__(connector, identity,
+                                           redfish_version)
+
+
+class BiosSettings(base.ResourceBase):
+
+    BootMode = base.Field(["Attributes", "BootMode"])
+    def __init__(self, connector, identity, redfish_version=None):
+        """A class representing a BootResource
+
+        :param connector: A Connector instance
+        :param identity: The identity of the BootResource
+        :param redfish_version: The version of RedFish. Used to construct
+            the object according to schema of the given version.
+        """
+        super(BiosSettings, self).__init__(connector, identity,
+                                           redfish_version)
--- proliantutils/redfish/resources/system/system.py
+++ proliantutils/redfish/resources/system/system.py
@@ -14,8 +14,9 @@
 
 __author__ = 'HPE'
 
+from proliantutils.redfish.resources.system.bios import BiosResource
+from proliantutils.redfish.resources.system.bios import BiosSettings
 from sushy.resources.system import system
-
 
 class HPESystem(system.System):
     """Class that extends the functionality of System resource class
@@ -23,3 +24,20 @@ class HPESystem(system.System):
     This class extends the functionality of System resource class
     from sushy
     """
+    bios_odataid = base.Field(['Bios', '@odata.id'])
+    @property
+    def bios_resource(self):
+        odataid_val = HPESystem.bios_odataid._load(self.json, self)
+        bios_resource = BiosResource(
+            self._conn, odataid_val, redfish_version=self.redfish_version)
+        return bios_resource
+
+    @property
+    def bios_settings(self):
+        bios_odataid_val = HPESystem.bios_odataid._load(self.json, self)
+        bios_resource = BiosResource(self._conn, bios_odataid_val,
+                                     redfish_version=self.redfish_version)
+        bios_settings_uri = bios_resource.bios_settings_odataid
+        bios_settings = BiosSettings(self._conn, bios_settings_uri,
+                                     redfish_version=self.redfish_version)
+        return bios_settings 
