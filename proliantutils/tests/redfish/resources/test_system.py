22612a16293ed03c897f0b82dc5152c849c282a4
kesper <paresh.h.sao@gmail.com>
2017-05-25 10:16:25 +0000

    Redfish: Adding the ability to get pending boot mode
    This commit adds new function 'get_pending_boot_mode' to
    get pending boot mode for server.
    Change-Id: If4f2b5ef9fff1c25b233621004c1a4efa2dd0f64

--- proliantutils/tests/redfish/resources/test_system.py
+++ proliantutils/tests/redfish/resources/test_system.py
@@ -30,7 +30,8 @@ class HPESystemTestCase(testtools.TestCase):
         self.conn = mock.MagicMock()
         with open('proliantutils/tests/redfish/'
                   'json_samples/system.json', 'r') as f:
-            self.conn.get.return_value.json.return_value = json.loads(f.read())
+            system_json = json.loads(f.read())
+        self.conn.get.return_value.json.return_value = system_json['Default']
 
         self.sys_inst = system.HPESystem(
             self.conn, '/redfish/v1/Systems/1',
