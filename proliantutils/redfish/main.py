3ff2a0582ff259b3dabb319dd4f25760b29add67
vmud213 <vinay50muddu@yahoo.com>
2017-05-25 12:33:06 +0530

    Add HPE extensions support to redfish connection lib
    This commit adds support to have HPE root and system
    resource extensions to redfish connection library.
    
    Partial-Bug: #1691955
    Change-Id: I65bf78d6471a5246465e0a6b226844748888ca73

---
+++ proliantutils/redfish/main.py
@@ -0,0 +1,35 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+__author__ = 'HPE'
+
+from proliantutils.redfish.resources.system import system
+import sushy
+
+
+class HPESushy(sushy.Sushy):
+    """Class that extends base Sushy class
+
+    This class extends the Sushy class to override certain methods
+    required to customize the functionality of different resources
+    """
+
+    def get_system(self, identity):
+        """Given the identity return a HPESystem object
+
+        :param identity: The identity of the System resource
+        :returns: The System object
+        """
+        return system.HPESystem(self._conn, identity,
+                                redfish_version=self.redfish_version)
