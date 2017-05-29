3c5b5ec62d6e6a0730d6befbeea5e46b40269c70
Anshul Jain <theanshuljain@gmail.com>
2017-05-29 06:30:21 +0000

    Adds 'activate_license' for redfish systems
    This commit provides functionality to activate license
    for redish based systems.
    
    Change-Id: I398dbf229bf7cef6fce2927f4098642dcff326b8

---
+++ proliantutils/redfish/resources/manager/manager.py
@@ -0,0 +1,34 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+__author__ = 'HPE'
+
+from sushy.resources.manager.manager import Manager
+
+
+class HPEManager(Manager):
+    """Class that extends the functionality of Manager resource class
+
+    This class extends the functionality of Manager resource class
+    from sushy
+    """
+    def set_license(self, lic_uri, lic_key):
+        """Set the license on a redfish system
+
+        :param lic_uri: path of license uri
+        :param lic_key: license key in dictionary format.
+        :returns: response object of the post operation
+        """
+        if lic_uri is not None:
+            return self._conn.post(lic_uri, data=lic_key)
