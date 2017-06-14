c608e6de80b49cf61fcf930eeb9ee05f2c8b9dc9
ankit <ankit.dhn31@gmail.com>
2017-06-14 12:51:06 +0000

    Adds 'reset_ilo_credentials' for redfish systems
    This commit provides functionality to update credentials
    on redish systems.
    
    Change-Id: I110f0780d1b894ce9cd7f06d32a39c3a7738e4b6

---
+++ proliantutils/redfish/resources/account_service/account.py
@@ -0,0 +1,42 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+__author__ = 'HPE'
+
+from sushy.resources import base
+
+
+def get_member_uri(username, members):
+    """Returns the uri of account to be updated
+
+    :param username: username of account
+    :param members: collection of accounts
+
+    :returns account uri
+    """
+    for member in members:
+        if member.username == username:
+            return member.path
+
+
+class HPEAccount(base.ResourceBase):
+
+    username = base.Field('UserName')
+
+
+class HPEAccountCollection(base.ResourceCollectionBase):
+
+    @property
+    def _resource_type(self):
+        return HPEAccount
