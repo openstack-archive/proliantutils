a7913eb54352b2f6fba0813b60a3899816507135
kesper <paresh.h.sao@gmail.com>
2017-07-06 11:37:34 +0000

    Redfish: Adding the ability to get persistent boot device
    This commits adds new function 'get_persistent_boot_device' to
    get persistent boot device for server.
    
    Change-Id: Id8fcec3a821d9ff003ff0f80fd33215c2c08a90d
    Depends-on: I85c1376457698bda81120b74f54e08e5236c9974

--- proliantutils/ilo/client.py
+++ proliantutils/ilo/client.py
@@ -68,6 +68,7 @@ SUPPORTED_REDFISH_METHODS = [
     'get_pending_boot_mode',
     'get_current_boot_mode',
     'activate_license',
+    'get_persistent_boot_device',
 ]
 
 LOG = log.get_logger(__name__)
