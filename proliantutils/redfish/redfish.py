013535516dbd812cd32531fa013f03db75192216
kesper <paresh.h.sao@gmail.com>
2017-07-10 11:48:21 +0000

    Redfish: Adding the ability to set boot mode and devices
    This commits adds new function 'set_pending_boot_mode',
    'update_persistent_boot' and 'set_one_time_boot' to
    the server
    
    Change-Id: I7609eac74143f4b529b011ed1cbab96b578770b8

--- proliantutils/redfish/redfish.py
+++ proliantutils/redfish/redfish.py
@@ -485,7 +485,83 @@ class RedfishOperations(operations.IloOperations):
             return PERSISTENT_BOOT_MAP.get(boot_device)
         except sushy.exceptions.SushyError as e:
             msg = (self._("The Redfish controller is unable to get "
-                          "persistent boot device.' Error %(error)s") %
+                          "persistent boot device. Error %(error)s") %
+                   {'error': str(e)})
+            LOG.debug(msg)
+            raise exception.IloError(msg)
+
+    def set_pending_boot_mode(self, boot_mode):
+        """Sets the boot mode of the system for next boot.
+
+        :param boot_mode: either 'uefi' or 'legacy'.
+        :raises: IloInvalidInputError, on an invalid input.
+        :raises: IloError, on an error from iLO.
+        """
+        sushy_system = self._get_sushy_system(PROLIANT_SYSTEM_ID)
+
+        if boot_mode.upper() not in BOOT_MODE_MAP_REV.keys():
+            msg = (('Invalid Boot mode specified, valid boot mode '
+                    'could be either uefi or legacy. Error: %(boot_mode)s')
+                   % {'boot_mode': boot_mode})
+            raise exception.IloInvalidInputError(msg)
+
+        try:
+            sushy_system.bios_settings.pending_settings.set_pending_boot_mode(
+                BOOT_MODE_MAP_REV.get(boot_mode.upper()))
+        except sushy.exceptions.SushyError as e:
+            msg = (self._('The Redfish controller failed to set '
+                          'pending boot mode. Error: %(error)s') %
+                   {'error': str(e)})
+            LOG.debug(msg)
+            raise exception.IloError(msg)
+
+    def update_persistent_boot(self, device_type=[], mac=None):
+        """Changes the persistent boot device order for the host
+
+        :param device_type: ordered list of boot devices
+        :param mac: intiator mac address, mandatory for iSCSI uefi boot
+        :raises: IloError, on an error from iLO.
+        :raises: IloInvalidInputError, if the command is not supported
+                 on the server.
+        """
+        sushy_system = self._get_sushy_system(PROLIANT_SYSTEM_ID)
+        # Check if the input is valid
+        for item in device_type:
+            if item.upper() not in DEVICE_COMMON_TO_REDFISH:
+                raise exception.IloInvalidInputError("Invalid input. Valid "
+                                                     "devices: NETWORK, HDD,"
+                                                     " ISCSI or CDROM.")
+        try:
+            sushy_system.update_persistent_boot(
+                device_type, persistent=True, mac=mac)
+        except sushy.exceptions.SushyError as e:
+            msg = (self._('The Redfish controller failed to update '
+                          'persistent boot. Error: %(error)s') %
+                   {'error': str(e)})
+            LOG.debug(msg)
+            raise exception.IloError(msg)
+
+    def set_one_time_boot(self, device, mac=None):
+        """Configures a single boot from a specific device.
+
+        :param device: Device to be set as a one time boot device
+        :param mac: intiator mac address, optional parameter
+        :raises: IloError, on an error from iLO.
+        :raises: IloInvalidInputError, if the command is not supported
+                 on the server.
+        """
+        sushy_system = self._get_sushy_system(PROLIANT_SYSTEM_ID)
+        # Check if the input is valid
+        if device.upper() not in DEVICE_COMMON_TO_REDFISH:
+            raise exception.IloInvalidInputError("Invalid input. Valid "
+                                                 "devices: NETWORK, HDD,"
+                                                 " ISCSI or CDROM.")
+        try:
+            sushy_system.update_persistent_boot(
+                [device], persistent=False, mac=mac)
+        except sushy.exceptions.SushyError as e:
+            msg = (self._('The Redfish controller failed to set '
+                          'one time boot. Error: %(error)s') %
                    {'error': str(e)})
             LOG.debug(msg)
             raise exception.IloError(msg)
