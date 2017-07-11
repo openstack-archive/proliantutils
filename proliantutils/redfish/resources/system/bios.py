4df97a8b89f55b48d003d341386576ead0937f3d
Nisha Agarwal <agarwalnisha1980@gmail.com>
2017-07-11 04:18:20 +0000

    Redfish: Add trusted_boot capability
    Change-Id: I0df4424f4030a13d25236018ec9f066217733be9

--- proliantutils/redfish/resources/system/bios.py
+++ proliantutils/redfish/resources/system/bios.py
@@ -36,6 +36,7 @@ class BIOSSettings(base.ResourceBase):
 
     boot_mode = base.MappedField(["Attributes", "BootMode"],
                                  mappings.GET_BIOS_BOOT_MODE_MAP)
+    tpm_state = base.Field(["Attributes", "TpmState"])
     _pending_settings = None
     _boot_settings = None
     _base_configs = None
