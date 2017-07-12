35348cf24a95974fe2aa4a8dbe755ac63dd60dcd
Nisha Agarwal <agarwalnisha1980@gmail.com>
2017-07-12 23:06:46 +0000

    Redfish: Adds macs discovery
    This patch adds EthernetInterfaces subresource.
    It returns the data in the form of a dictionary as
    {"aa:bb:cc:dd:ee:ff" : "Enabled"}
    
    Change-Id: I123bdc4aae68bbbdf9b637746aa4d7f109d12b03

--- proliantutils/redfish/resources/system/mappings.py
+++ proliantutils/redfish/resources/system/mappings.py
@@ -58,3 +58,20 @@ SECUREBOOT_RESET_KEYS_MAP = {
 
 SECUREBOOT_RESET_KEYS_MAP_REV = (
     utils.revert_dictionary(SECUREBOOT_RESET_KEYS_MAP))
+
+HEALTH_STATE_VALUE_MAP = {
+    'Enabled': constants.HEALTH_STATE_ENABLED,
+    'Disabled': constants.HEALTH_STATE_DISABLED,
+}
+
+HEALTH_STATE_VALUE_MAP_REV = (
+    utils.revert_dictionary(HEALTH_STATE_VALUE_MAP))
+
+HEALTH_VALUE_MAP = {
+    'OK': constants.HEALTH_OK,
+    'Warning': constants.HEALTH_WARNING,
+    'Critical': constants.HEALTH_CRITICAL
+}
+
+HEALTH_VALUE_MAP_REV = (
+    utils.revert_dictionary(HEALTH_VALUE_MAP))
