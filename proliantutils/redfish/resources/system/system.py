07b27be2dc534b48ac522042732df33f4e046547
Nisha Agarwal <agarwalnisha1980@gmail.com>
2017-07-12 23:06:46 +0000

    Redfish: Adds macs discovery
    This patch adds EthernetInterfaces subresource.
    It returns the data in the form of a dictionary as
    {"aa:bb:cc:dd:ee:ff" : "Enabled"}
    
    Change-Id: I123bdc4aae68bbbdf9b637746aa4d7f109d12b03

--- proliantutils/redfish/resources/system/system.py
+++ proliantutils/redfish/resources/system/system.py
@@ -21,6 +21,8 @@ from sushy.resources.system import system
 from proliantutils import exception
 from proliantutils import log
 from proliantutils.redfish.resources.system import bios
+from proliantutils.redfish.resources.system import constants as sys_cons
+from proliantutils.redfish.resources.system import ethernet_interface
 from proliantutils.redfish.resources.system import mappings
 from proliantutils.redfish import utils
 
@@ -46,6 +48,10 @@ class HpeActionsField(base.CompositeField):
         PowerButtonActionField('#HpeComputerSystemExt.PowerButton'))
 
 
+class HpeEthernetInterface(base.CompositeField):
+    eth_interface_path = base.Field(['EthernetInterface', '@odata.id'])
+
+
 class HPESystem(system.System):
     """Class that extends the functionality of System resource class
 
@@ -58,6 +64,10 @@ class HPESystem(system.System):
     """Oem specific system extensibility actions"""
 
     _bios_settings = None
+
+    _ethernet_interfaces = None
+    _hpe_eth_interface = HpeEthernetInterface(['Oem', 'Hpe',
+                                               'EthernetInterfaces'])
 
     def _get_hpe_push_power_button_action_element(self):
         push_action = self._hpe_actions.computer_system_ext_powerbutton
@@ -141,3 +151,29 @@ class HPESystem(system.System):
         tenure = (sushy.BOOT_SOURCE_ENABLED_CONTINUOUS
                   if persistent else sushy.BOOT_SOURCE_ENABLED_ONCE)
         self.set_system_boot_source(device, enabled=tenure)
+
+    def _get_hpe_sub_resource_collection_path(self, sub_res):
+        path = None
+        try:
+            path = utils.get_subresource_path_by(self, sub_res)
+        except exception.MissingAttributeError:
+                path = utils.get_subresource_path_by(
+                    self, ['Oem', 'Hpe', 'Links', sub_res])
+        return path
+
+    @property
+    def ethernet_interfaces(self):
+        """Provide reference to EthernetInterfacesCollection instance"""
+        if self._ethernet_interfaces is None:
+            sub_res = 'EthernetInterfaces'
+            self._ethernet_interfaces = (
+                ethernet_interface.EthernetInterfaceCollection(
+                    self._conn,
+                    self._get_hpe_sub_resource_collection_path(sub_res),
+                    redfish_version=self.redfish_version))
+
+        return self._ethernet_interfaces
+
+    def refresh(self):
+        super(HPESystem, self).refresh()
+        self._ethernet_interfaces = None
