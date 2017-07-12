252ed3c5b6b985d77cd30e331517ddbcaa9e2c17
Nisha Agarwal <agarwalnisha1980@gmail.com>
2017-07-12 23:58:31 +0000

    Redfish: Adds local_gb discovery
    Change-Id: Iff415e0ed83c546469d70e25318d48c361c765a8

---
+++ proliantutils/redfish/resources/system/unconfigured_drives.py
@@ -0,0 +1,64 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from sushy.resources import base
+
+LOG = logging.getLogger(__name__)
+
+
+class HealthStatusField(base.CompositeField):
+    health = base.Field('Health')
+    state = base.Field('State')
+
+
+class UnconfiguredDrive(base.ResourceBase):
+
+    identity = base.Field('Id', required=True)
+
+    name = base.Field('Name')
+
+    description = base.Field('Description')
+
+    status = HealthStatusField('Status')
+
+    capacity_mib = base.Field('CapacityMiB')
+
+    capacity_gb = base.Field('CapacityiGB')
+
+    media_type = base.Field('MediaType')
+
+    rotational_speed_rpm = base.Field('RotationalSpeedRpm')
+
+    legacy_boot_priority = base.Field('LegacyBootPriority')
+
+    encrypted_drive = base.Field('EncryptedDrive')
+
+
+class UnconfiguredCollection(base.ResourceCollectionBase):
+
+    @property
+    def _resource_type(self):
+        return UnconfiguredDrive
+
+    _size = None
+
+    @property
+    def size(self):
+        _size = 0
+        for mem in self.members:
+            if _size < mem.capacity_gb:
+                _size = mem.capacity_gb
+        return (_size * 1024 * 1024 * 1024)
