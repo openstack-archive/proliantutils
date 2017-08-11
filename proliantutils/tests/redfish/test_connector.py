3edca4e215c5a740e8dda1e19d2abe924d890d36
vmud213 <vinay50muddu@yahoo.com>
2017-08-11 12:26:48 +0530

    Redfish: Adds HPEConnector to proliantutils
    Implements HPEConnector class that overrides the '_op' instance method
    from sushy's Connector class to include retrying logic. The retrying
    logic tries for MAX_RETRY_ATTEMPTS attempts waiting for MAX_TIME_BEFORE_RETRY
    time after each retry. Also adds constructor to HPESushy class that bypasses
    the initialization of the Sushy class and initialize the ResourceBase class
    with customized HPE specific connector subtype.
    
    Change-Id: Ic11552ed12195206717e9fcfaffa4c6d20bc6543
    Closes-Bug: #1710071

---
+++ proliantutils/tests/redfish/test_connector.py
@@ -0,0 +1,45 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import retrying
+
+import mock
+import testtools
+
+from oslo_utils import importutils
+from sushy import connector
+
+
+class HPEConnectorTestCase(testtools.TestCase):
+
+    def setUp(self):
+        super(HPEConnectorTestCase, self).setUp()
+        self.retry_backup = retrying.retry
+        retrying.retry = mock.MagicMock(return_value=lambda x: x, spec_set=[])
+        hpe_connector = importutils.try_import(
+            'proliantutils.redfish.connector')
+        self.conn = hpe_connector.HPEConnector(
+            'http://foo.bar:1234', username='user',
+            password='pass', verify=True)
+        self.data = {'fake': 'data'}
+        self.headers = {'X-Fake': 'header'}
+
+    @mock.patch.object(connector.Connector, '_op', autospec=True)
+    def test__op(self, conn_mock):
+        self.assertEqual(id(self.conn._op), id(connector.Connector._op))
+
+    def tearDown(self):
+        super(HPEConnectorTestCase, self).tearDown()
+        retrying.retry = self.retry_backup
