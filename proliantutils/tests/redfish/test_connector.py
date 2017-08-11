3a6aece227f9c0649cef861b7eb8458400ddd819
vmud213 <vinay50muddu@yahoo.com>
2017-08-11 12:26:48 +0530

    Redfish: Adds HPEConnector to proliantutils
    Implements HPEConnector class that overrides the '_op' instance method
    from sushy's Connector class to include retrying logic. The retrying
    logic tries for MAX_RETRY_ATTEMPTS attempts waiting for MAX_TIME_BEFORE_RETRY
    time after each retry. Also adds constructor to HPESushy class that bypasses
    the initialization of the Sushy class and initialize the ResourceBase class
    with customized HPE specific connector subtype.
    
    Change-Id: Ic11552ed12195206717e9fcfaffa4c6d20bc6543
    Closes-Bug: #1710071

---
+++ proliantutils/tests/redfish/test_connector.py
@@ -0,0 +1,63 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import retrying
+import sys
+
+import mock
+import testtools
+
+from oslo_utils import importutils
+
+
+class HPEConnectorTestCase(testtools.TestCase):
+
+    def setUp(self):
+        super(HPEConnectorTestCase, self).setUp()
+        self.retry_backup = retrying.retry
+        self.imp_str = 'proliantutils.redfish.connector'
+
+    def test__op(self):
+        retrying.retry = mock.MagicMock(return_value=lambda x: x, spec_set=[])
+        try:
+            sys.modules.pop(self.imp_str)
+        except KeyError:
+            pass
+        hpe_connector = importutils.try_import(
+            self.imp_str)
+        conn = hpe_connector.HPEConnector(
+            'http://foo.bar:1234', username='user',
+            password='pass', verify=True)
+        self.assertEqual(id(conn.__class__._op),
+                         id(hpe_connector.HPEConnector._op))
+
+    def test__op_retry(self):
+        retrying.retry = mock.MagicMock(return_value=lambda x: "Hello",
+                                        spec_set=[])
+        try:
+            sys.modules.pop(self.imp_str)
+        except KeyError:
+            pass
+        hpe_connector = importutils.try_import(
+            self.imp_str)
+        self.assertEqual("Hello", hpe_connector.HPEConnector._op)
+
+    def tearDown(self):
+        super(HPEConnectorTestCase, self).tearDown()
+        retrying.retry = self.retry_backup
+        try:
+            sys.modules.pop(self.imp_str)
+        except KeyError:
+            pass
