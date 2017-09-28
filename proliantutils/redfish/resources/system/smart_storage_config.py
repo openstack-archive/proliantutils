39b34a0a2dae1c6e0f0b820c01227175e36378f9
Anshul Jain <theanshuljain@gmail.com>
2017-09-28 14:42:55 +0530

    Redfish: Adds 'delete_raid_configuration' API to clear raid
    The commit adds functionality to delete logical drives in a
    SmartStorageConfig redfish systems.
    
    Co-Authored-By: Paresh Sao <paresh.sao@hpe.com>
    Change-Id: Id3a07ab0ca1c0ad8199a28ef2efb22aadc5edfd5

---
+++ proliantutils/redfish/resources/system/smart_storage_config.py
@@ -0,0 +1,50 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from sushy.resources import base
+
+
+class HPESmartStorageConfig(base.ResourceBase):
+    """Class that defines the functionality for SmartSorageConfig Resources."""
+
+    physical_drives = base.Field("PhysicalDrives", adapter=list)
+
+    settings_uri = base.Field(["@Redfish.Settings",
+                               "SettingsObject", "@odata.id"])
+
+    def delete_raid(self):
+        """Clears the RAID configuration from the system."""
+        if self.physical_drives is None:
+            return
+
+        data = {
+            "LogicalDrives": [],
+            "DataGuard": "Disabled",
+            "PhysicalDrives": self.physical_drives,
+            "Actions": [{"Action": "FactoryReset"}]
+        }
+        self._conn.put(self.settings_uri, data=data)
