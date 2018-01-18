459ed0ac0a2a2ebdf0b98ea3c30461b4a6febc7d
Anshul Jain <theanshuljain@gmail.com>
2018-01-18 04:14:52 -0600

    [WIP]Redfish: Adds 'read_raid_configuration' API to get current raid config
    This commit adds functionality to verify the success of raid create
    or delete operation and gets the current raid configuration on
    SmartStorageConfig redfish systems.
    
    Change-Id: I9f546e8ef87229f442dc3855042cbe3ac3a198f8

--- proliantutils/redfish/resources/system/smart_storage_config.py
+++ proliantutils/redfish/resources/system/smart_storage_config.py
@@ -16,11 +16,14 @@ import json
 
 from sushy.resources import base
 
+from proliantutils import exception
 from proliantutils.redfish import utils
 
 
 class HPESmartStorageConfig(base.ResourceBase):
     """Class that defines the functionality for SmartSorageConfig Resources."""
+
+    smart_storage_config_message = base.Field(['@Redfish.Settings', 'Messages'])
 
     def _get_smart_storage_config_url(self):
         """Get the smart storage config settings url.
@@ -29,6 +32,58 @@ class HPESmartStorageConfig(base.ResourceBase):
         """
         return utils.get_subresource_path_by(self, ["@Redfish.Settings",
                                                     "SettingsObject"])
+
+    def _generic_format(self, raid_config):
+        """Converts the redfish data of current raid configuration to generic
+        key-value format to keep in sync with inband cleaning output.
+
+        :returns: current raid config.
+        """
+        logical_drives = raid_config["LogicalDrives"]
+        logical_disks = []
+        for ld in logical_drives:
+            prop = {}
+            prop['size_gb'] = ld['CapacityGiB']
+            prop['raid_level'] = ld['Raid'].strip('Raid')
+            prop['root_device_hint'] = {'wwn': '0x' +
+                                        ld['VolumeUniqueIdentifier']}
+            prop['controller'] = ("Smart Storage Controller in " +
+                                  raid_config["Location"])
+            prop['physical_disks'] = ld['DataDrives']
+            prop['volume_name'] = ld['LogicalDriveName']
+            logical_disks.append(prop)
+        return logical_disks
+
+    def _check_smart_storage_message(self):
+        """Check for smart storage message.
+
+        :returns: result, raid_message
+        """
+        ssc_mesg = self.smart_storage_config_message
+        result = True
+        raid_message = ""
+        for element in ssc_mesg:
+            if "Success" not in element['MessageId'] :
+                result = False
+                raid_message = element['MessageId']
+        return result, raid_message
+
+    def read_raid(self):
+        """Gets the current RAID configuration from the system.
+
+        :returns: current raid config.
+        """
+        result, raid_message = self._check_smart_storage_message()
+        if result:
+            raid_data = {}
+            raid_config = self._conn.get(self._get_smart_storage_config_url())
+            raid_data['logical_disks'] = self._generic_format(raid_config.json())
+            return raid_data
+        else:
+            msg = ('Failed to perform the raid operation successfully.'
+                   'Response from controller Error - %(error)s'
+                   % {'error': str(raid_message)})
+            raise exception.IloError(msg)
 
     def delete_raid(self):
         """Clears the RAID configuration from the system."""
