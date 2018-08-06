a8dee79360064edc7afff02b404af3d76e190a08
vmud213 <vinay50muddu@yahoo.com>
2018-08-06 21:57:54 +0530

    Validates the BIOS settings before applying
    If 'only_allowed_settings' filter is applied, this change validates if
    each of the settings provided is supported. If any setting is provided which
    is not supported by the user, 'IloError' exception is thrown.
    
    Change-Id: I966db94634b7e3021f07dc45dcca5cc4f5120329

--- proliantutils/ilo/ris.py
+++ proliantutils/ilo/ris.py
@@ -1924,14 +1924,21 @@ class RISOperations(rest.RestConnectorBase, operations.IloOperations):
         :raises: IloCommandNotSupportedError, if the command is not supported
                  on the server.
         """
-        if only_allowed_settings and data:
-            refined_data = utils.apply_bios_properties_filter(
-                data, constants.SUPPORTED_BIOS_PROPERTIES)
-            self._change_bios_setting(refined_data)
+        if not data:
             return
 
-        if data:
-            self._change_bios_setting(data)
+        refined_data = data
+        if only_allowed_settings:
+            unsupported_settings = [key for key in data if key not in (
+                constants.SUPPORTED_BIOS_PROPERTIES)]
+            if unsupported_settings:
+                msg = ("Could not apply settings as one or more settings are"
+                       " not supported.%s" % (unsupported_settings))
+                raise exception.IloError(msg)
+            refined_data = utils.apply_bios_properties_filter(
+                data, constants.SUPPORTED_BIOS_PROPERTIES)
+
+        self._change_bios_setting(refined_data)
 
     def get_default_bios_settings(self, only_allowed_settings=True):
         """Get default BIOS settings.
